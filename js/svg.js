e5534d9a046d6d23a8f7b565b3e5541702b31b85
Guðni Natan Gunnarsson <1493259+GudniNatan@users.noreply.github.com>
2017-08-31 17:06:36 +0000

    Virkar!!

--- js/svg.js
+++ js/svg.js
@@ -1,30 +1,56 @@
-function handleOrientation(event) {
-  let absolute = event.absolute;
-  let alpha    = event.alpha || 30;
-  let beta     = event.beta || 20;
-  let gamma    = event.gamma || 10;
-
-  // Do stuff with the new orientation data
-  let compassContainer = document.querySelector('.compass');
-  compassContainer.style.top = alpha;
-  compassContainer.style.left = beta;
-
-  let infobox = document.getElementById('info');
-  infobox.innerHTML = `absolute ${absolute}, alpha ${alpha}, beta ${beta}, gamma ${gamma}`;
-}
-
-let alpha = 1;
-let beta = 1;
-let gamma = 1;
-let absolute = 1;
-
-let compassContainer = document.querySelector('.compass');
-compassContainer.style.top = alpha;
-compassContainer.style.left = beta;
+let alphaRandom = Math.round(Math.random() * 360);
 
 let infobox = document.getElementById('info');
-infobox.innerHTML = "sup"
+infobox.innerHTML = "sup";
 
-console.log("hello");
+let comp = document.getElementById("svg8");
+comp.style.display = 'none';
 
-window.addEventListener("deviceorientation", handleOrientation, true);
+let handleOrientationFunction = mainHandleOrientation;
+
+function mainHandleOrientation(event) {
+  	let absolute = event.absolute || false;
+  	let alpha    = event.alpha || 30;
+  	let beta     = event.beta || 20;
+	let gamma    = event.gamma || 10;
+	if (!absolute) //if alpha is not absolute, it is randomized.
+	{
+		alpha = (alphaRandom + alpha) % 360;
+	}
+
+  	// Do stuff with the new orientation data
+	comp.style.display = 'block';
+
+  	comp.style.transform = `rotate(${alpha}deg) translateY(0%)`;
+
+  	let needle = comp.getElementById("path4609");
+  	let needleColor = Math.floor(Math.abs(180-alpha) / 180 * 9) + 1;
+  	if (alpha < 180){
+  		needleColor = Math.floor(Math.abs(alpha-180) / 180 * 9) + 1;
+  	}
+  	needle.style.fill = `${needleColor}${needleColor}0000`;
+
+  	let infobox = document.getElementById('info');
+  	infobox.innerHTML = `absolute ${absolute}, alpha ${alpha}, beta ${beta}, gamma ${gamma}, color ${needleColor}`;
+  	if (alpha > 355 || alpha < 5) {
+  		alert("Vel gert!");
+  		console.log("done!");
+  		handleOrientationFunction = rotated;
+  		comp.style.transition = "300ms ease all";
+  		if (alpha > 180){
+  			comp.style.transform = `rotate(360deg) translateY(0%)`;
+  		}
+  		else{
+  			comp.style.transform = `rotate(0deg) translateY(0%)`;
+  		}
+		comp.getElementById("path4609").style.fill = "900000";
+		console.log("rotated!");
+  	}
+}
+function rotated(event) {
+	//Do whatever
+}
+
+window.addEventListener("deviceorientation", function dev_or_func(event) {
+	handleOrientationFunction(event);
+}, true);
