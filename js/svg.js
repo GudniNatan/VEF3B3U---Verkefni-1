d56641b3da65973612d2d5dfa4696d43836390c8
Guðni Natan Gunnarsson <1493259+GudniNatan@users.noreply.github.com>
2017-09-03 23:02:46 +0000

    Hreinna og betra

--- js/svg.js
+++ js/svg.js
@@ -1,17 +1,22 @@
 let alphaRandom = Math.round(Math.random() * 360);
 
 let infobox = document.getElementById('info');
-infobox.innerHTML = "sup";
+infobox.textContent = "This device/browser is not supported.";
 
 let comp = document.getElementById("svg8");
-comp.style.display = 'none';
+//comp.style.display = 'none';
 
-let handleOrientationFunction = mainHandleOrientation;
+drawLineAnimation(document.getElementById('path3711'));
+drawLineAnimation(document.getElementById('path3712'));
+drawLineAnimation(document.getElementById('path3713'));
+drawLineAnimation(document.getElementById('path3714'));
+
+handleOrientationFunction = mainHandleOrientation;
 
 function mainHandleOrientation(event) {
-  	let absolute = event.absolute || false;
-  	let alpha    = event.alpha || 30;
-  	let beta     = event.beta || 20;
+  let absolute = event.absolute || false;
+  let alpha    = event.alpha || 30;
+  let beta     = event.beta || 20;
 	let gamma    = event.gamma || 10;
 	if (!absolute) //if alpha is not absolute, it is randomized.
 	{
@@ -43,13 +48,41 @@ function mainHandleOrientation(event) {
   		else{
   			comp.style.transform = `rotate(0deg) translateY(0%)`;
   		}
-		comp.getElementById("path4609").style.fill = "900000";
-		console.log("rotated!");
+  		needle.style.fill = "900000";
+  		console.log("rotated!");
   	}
 }
 function rotated(event) {
 	//Do whatever
+  let absolute = event.absolute || false;
+  let alpha    = event.alpha || 30;
+  let beta     = event.beta || 20;
+  let gamma    = event.gamma || 10;
+  if (!absolute) //if alpha is not absolute, it is randomized.
+  {
+    alpha = (alphaRandom + alpha) % 360;
+  }
+  let infobox = document.getElementById('info');
+  infobox.innerHTML = `absolute ${absolute}, alpha ${alpha}, beta ${beta}, gamma ${gamma}, color full`;
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function drawLineAnimation(path){
+  let pathLength = path.getTotalLength();
+  path.style.strokeDasharray = pathLength.toString() + ' ' + pathLength.toString();
+  path.style.strokeDashoffset = pathLength;
+  path.style.opacity = 0;
+
+  await sleep(25);
+  path.style.transition = "all 2s ease-in";
+
+  path.style.strokeDashoffset = '0';
+  path.style.opacity = 1;
+}
+
 
 window.addEventListener("deviceorientation", function dev_or_func(event) {
 	handleOrientationFunction(event);
